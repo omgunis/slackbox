3d1ab2305a8554fcc370a3de52a64d7ab08b2155
omgunis <eunicegchang@gmail.com>
2017-05-24 18:07:16 -0700

    testing 1

--- app.js
+++ app.js
@@ -80,9 +80,9 @@ app.post('/store', function(req, res) {
           spotifyApi.getPlaylistTracks(process.env.SPOTIFY_USERNAME,  process.env.SPOTIFY_PLAYLIST_ID, { 'offset' : 1, 'limit' : 5, 'fields' : 'items' })
             .then(function(data) {
               var message = 'data.body.items[0].track.artists[0].name' + data.body.items[0].track.artists[0].name + ' - *' + 'data.body.items[0].track.name' + data.body.items[0].track.name + '*';
-              var message2 = 'data.body.items[1].track.artists[0].name' + data.body.items[1].track.artists[0].name + ' - *' + 'data.body.items[1].track.name' + data.body.items[1].track.name + '*';
-              var message3 = 'data.body.items[0].track.artists[1].name' + data.body.items[0].track.artists[1].name + ' - *' + 'data.body.items[1].track.name' + data.body.items[1].track.name + '*';
-              var items = data.body.items;
+              // var message2 = 'data.body.items[1].track.artists[0].name' + data.body.items[1].track.artists[0].name + ' - *' + 'data.body.items[1].track.name' + data.body.items[1].track.name + '*';
+              // var message3 = 'data.body.items[0].track.artists[1].name' + data.body.items[0].track.artists[1].name + ' - *' + 'data.body.items[1].track.name' + data.body.items[1].track.name + '*';
+              // var items = data.body.items;
               // for (var key in items){
               //   if(items.hasOwnProperty(key)){
               //     var artists = items[key].track.artists[0].name
