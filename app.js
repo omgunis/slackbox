c2e7c76c2299782d4af95e7449c7655ea1ca0dec
omgunis <eunicegchang@gmail.com>
2017-05-24 17:36:08 -0700

    Parse through json data to hopefully print artist

--- app.js
+++ app.js
@@ -77,10 +77,10 @@ app.post('/store', function(req, res) {
           )
         }
         else if(text === 'listtracks'){
-          var message = 'Current tracks: ';
           spotifyApi.getPlaylistTracks(process.env.SPOTIFY_USERNAME,  process.env.SPOTIFY_PLAYLIST_ID, { 'offset' : 1, 'limit' : 5, 'fields' : 'items' })
             .then(function(data) {
-              return slack(res, message + JSON.stringify(data.body, 4));
+              var message = 'data.body.items.track.artists.name  ' + data.body.items.track.artists.name + 'data.body.items.track.name ' + data.body.items.track.name;
+              return slack(res, message);
             }, function(err) {
               return slack(res, err.message);
             });
@@ -134,3 +134,87 @@ app.post('/store', function(req, res) {
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
+
+// 
+// {
+//   "items":[
+//     {
+//       "added_at":"2017-05-25T00:11:34Z",
+//       "added_by":{
+//         "external_urls":{
+//           "spotify":"http://open.spotify.com/user/cashmereeunice"
+//         },
+//         "href":"https://api.spotify.com/v1/users/cashmereeunice",
+//         "id":"cashmereeunice","type":"user","uri":"spotify:user:cashmereeunice"
+//       },
+//       "is_local":false,
+//       "track":{
+//         "album":{
+//           "album_type":"album",
+//           "artists":[{
+//             "external_urls":{
+//               "spotify":"https://open.spotify.com/artist/1RyvyyTE3xzB2ZywiAwp0i"
+//             },
+//             "href":"https://api.spotify.com/v1/artists/1RyvyyTE3xzB2ZywiAwp0i",
+//             "id":"1RyvyyTE3xzB2ZywiAwp0i",
+//             "name":"Future",
+//             "type":"artist",
+//             "uri":"spotify:artist:1RyvyyTE3xzB2ZywiAwp0i"
+//           }],
+//           "available_markets":[
+//             "AD","AR","AT","AU","BE","BG","BO","BR","CA","CH","CL","CO","CR","CY","CZ","DE","DK","DO","EC","EE","ES","FI","FR","GB","GR","GT","HK","HN","HU","ID","IE","IS","IT","JP","LI","LT","LU","LV","MC","MT","MX","MY","NI","NL","NO","NZ","PA","PE","PH","PL","PT","PY","SE","SG","SK","SV","TR","TW","US","UY"],
+//             "external_urls":{
+//               "spotify":"https://open.spotify.com/album/4YtTX4GPvBvewbJvBfXCS2"
+//             },
+//             "href":"https://api.spotify.com/v1/albums/4YtTX4GPvBvewbJvBfXCS2",
+//             "id":"4YtTX4GPvBvewbJvBfXCS2",
+//             "images":[{
+//               "height":640,
+//               "url":"https://i.scdn.co/image/31b0a579225a718b4b1aea0ac6f9e9bdd3da17b0",
+//               "width":640
+//             },
+//             {
+//               "height":300,
+//               "url":"https://i.scdn.co/image/6b311ec6e3d6999a14e7d1ea8841d00226c292c6",
+//               "width":300
+//             },
+//             {
+//               "height":64,
+//               "url":"https://i.scdn.co/image/5c132e86154f68bdf384cc165b07cb5dcb2ec5a3",
+//               "width":64
+//             }
+//             ],
+//             "name":"FUTURE",
+//             "type":"album",
+//             "uri":"spotify:album:4YtTX4GPvBvewbJvBfXCS2"
+//           },
+//           "artists":[{
+//             "external_urls":{
+//               "spotify":"https://open.spotify.com/artist/1RyvyyTE3xzB2ZywiAwp0i"
+//             },
+//             "href":"https://api.spotify.com/v1/artists/1RyvyyTE3xzB2ZywiAwp0i",
+//             "id":"1RyvyyTE3xzB2ZywiAwp0i",
+//             "name":"Future",
+//             "type":"artist",
+//             "uri":"spotify:artist:1RyvyyTE3xzB2ZywiAwp0i"
+//           }],
+//           "available_markets":["AD","AR","AT","AU","BE","BG","BO","BR","CA","CH","CL","CO","CR","CY","CZ","DE","DK","DO","EC","EE","ES","FI","FR","GB","GR","GT","HK","HN","HU","ID","IE","IS","IT","JP","LI","LT","LU","LV","MC","MT","MX","MY","NI","NL","NO","NZ","PA","PE","PH","PL","PT","PY","SE","SG","SK","SV","TR","TW","US","UY"],
+//           "disc_number":1,
+//           "duration_ms":204600,
+//           "explicit":true,"external_ids":{
+//             "isrc":"USSM11701444"
+//           },
+//           "external_urls":{
+//             "spotify":"https://open.spotify.com/track/3rOSwuTsUlJp0Pu0MkN8r8"
+//           },
+//           "href":"https://api.spotify.com/v1/tracks/3rOSwuTsUlJp0Pu0MkN8r8",
+//           "id":"3rOSwuTsUlJp0Pu0MkN8r8",
+//           "name":"Mask Off",
+//           "popularity":98,
+//           "preview_url":"https://p.scdn.co/mp3-preview/0519fc7c44d418ca6c721d9aa83e9597065288e1?cid=955a1b3c419d445e859940767769c36f",
+//           "track_number":7,
+//           "type":"track",
+//           "uri":"spotify:track:3rOSwuTsUlJp0Pu0MkN8r8"
+//         }
+//       }]
+//   }
